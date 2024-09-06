3c6a837fb08202ba7c7bbf35ed4ff5155516ce28
ringkasan.net <ringkasan.net@gmail.com>
2024-09-06 18:50:19 +0700

    Fix all

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,80 +1,85 @@
 <template>
   <main>
     <div class="container">
-      <canvas id="canvas1" width="1366" height="531"></canvas>
+      <canvas id="canvas1" width="500" height="531"></canvas>
     </div>
-    <button> Test Buttons </button>
+    <button @click="activate">Activate</button>
+    <button @click="stop">Stop</button>
   </main>
 </template>
 
 <script setup>
 //Imports
 import * as rive from "@rive-app/canvas";
-import {ref, onMounted, useTemplateRef} from 'vue'
+import { ref, onMounted, useTemplateRef } from "vue";
 
-
-
-//Defining Variables and Functions
-//Variables
-  //Layouts
-let layout = new rive.Layout({
-        fit: rive.Fit.Cover,
-        alignment: rive.Alignment.Center,
-        });
-  
-let file = new rive.RiveFile({ 
-  src: 'cubetransition (1).riv',
-  onLoad: () => {} })
-let activeBool = ref(false);
 const stageNum = ref(1);
-const canvas = document.getElementById('canvas1');
 
-//Functions
-function loadRiveFile(src) {
-  file = new rive.RiveFile({
+async function loadRiveFile(src) {
+  const file = new rive.RiveFile({
     src: src,
-    onLoad: () => {}
-  });
-  file.init();
-  
-}
-
-function playCube(){
-  activeBool.value = true
-}
-
-
-
-//Main Codes
-onMounted(() => {
-console.log("hi tambah")
-console.log("halo ini evan yang tambah")
-  const cubeRive = new rive.Rive({
-    riveFile : file,
-    canvas : canvas,
-    layout : layout,
-    stateMachine: 'State Machine 1',
-    autoplay : true,
-    artboard: 'Artboard 2',
     onLoad: () => {
-      // Prevent a blurry canvas by using the device pixel ratio
-      cubeRive.resizeDrawingSurfaceToCanvas();
-
-      const inputs = cubeRive.stateMachineInputs('State Machine 1');
-      const activeBoolInside = inputs.find(i => i.name === 'Activate');
-      activeBoolInside.value = activeBool.value
-      console.log(activeBool)
-      const stageNumInside = inputs.find(i => i.name === 'Stage');
-      stageNumInside.value = stageNum.value
-      console.log(stageNum)
-      
+      console.log("File Loaded");
     },
-  })
+    onLoadError: (error) => {
+      console.error("Error loading file: " + error);
+    },
+  });
+  await file.init();
+  return file;
+}
 
+function playCube() {
+  activeBool.value = true;
+}
 
+let cubeFile;
+let canvas;
+const layout = new rive.Layout({
+  fit: rive.Fit.Contain,
+  alignment: rive.Alignment.TopCenter,
 });
 
+//Main Codes
+function activate() {
+  activeBoolController.value = true;
+}
 
+function stop() {
+  activeBoolController.value = false;
+}
 
+let activeBoolController;
+let activeBoolState = ref(false);
 
+onMounted(async () => {
+  cubeFile = await loadRiveFile("cubetransition.riv");
+  canvas = document.getElementById("canvas1");
+  const cubeRive=setupRive(cubeFile, canvas, "State Machine 1", "Artboard 2", () => {
+    // Prevent a blurry canvas by using the device pixel ratio
+    cubeRive.resizeDrawingSurfaceToCanvas();
+    const inputs = cubeRive.stateMachineInputs(stateMachineName);
+    activeBoolController = inputs.find((i) => i.name === "Activate");
+    console.log(activeBoolController.value);
+    const stageNumInside = inputs.find((i) => i.name === "Stage");
+    stageNumInside.value = stageNum.value;
+    console.log(stageNum.value);
+  });
+
+  console.log("hi");
+  console.log("halo ini evan yang tambah");
+});
+
+function setupRive(file, canvas, stateMachineName, artboardName, onLoad) {
+  const rive_ = new rive.Rive({
+    riveFile: file,
+    canvas: canvas,
+    layout: layout,
+    stateMachines: stateMachineName,
+    artboard: artboardName,
+    autoplay: true,
+    onLoad: onLoad,
+  });
+  return rive_;
+}
 </script>

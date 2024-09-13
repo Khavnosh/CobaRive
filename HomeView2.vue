4063c8c495b03357f5bfd5b3d19a0ff3591d02c3
Khavnosh <geraldjkarim1025@gmail.com>
2024-09-13 22:55:38 +0700

    Rewriting HomeView (progress 2)

--- HomeView2.vue
+++ HomeView2.vue
@@ -16,8 +16,10 @@ const layout = new rive.Layout({
   alignment: rive.Alignment.TopCenter,
 });
 
-let activeBoolController = ref('');
+let activeBoolController;
+let activeBoxController;
 let cubeFile;
+let cube = ref(true);
 
 //Functions
 async function loadRiveFile(src){
@@ -36,10 +38,32 @@ function activate(){
     activeBoolController.value = true
 }
 
+function switchFile(){
+    if(cube.value = true){
+        cube.value = false;
+        const riveSet = setupRive(cubeFile, "Artboard 1", "State Machine 1", () => {
+        riveSet.resizeDrawingSurfaceToCanvas;
+        const inputs = riveSet.stateMachineInputs("State Machine 1");
+        activeBoolController = inputs.find((i) => i.name === "Activate");
+    } )
+    }
+    else{
+        cube.value = true;
+        const riveSet1 = setupRive(cubeFile, "Artboard 1", "State Machine 1", () => {
+            riveSet1.resizeDrawingSurfaceToCanvas;
+            const input = riveSet1.stateMachineInputs("State Machine 1");
+            activeBoxController = inputs.find((i) => i.name === "Bool");
+            activeBoxController.value = false;
+
+    } )
+    }
+}
+
 //Main Code
 onMounted( async () => {
     const canvas1 = document.getElementById('canvas1');
     cubeFile = await loadRiveFile("cubetransition (1).riv");
+    boxFile = await loadRiveFile("box.riv");
     const riveSet = setupRive(cubeFile, "Artboard 1", "State Machine 1", () => {
         riveSet.resizeDrawingSurfaceToCanvas;
         const inputs = cubeRive.stateMachineInputs("State Machine 1");
